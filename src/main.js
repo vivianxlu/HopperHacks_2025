54c6afe6a465fcb43dea5b4a67130b5d1f5ec8a5
SHUYESHUI\lzhen <57676919+LZheng47@users.noreply.github.com.>
2025-02-22 13:31:40 -0500

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes={
+  width: window.innerWidth,
+  height:window.innerHeight,
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas
+}
+
+const game = new Phaser.Game(config)
