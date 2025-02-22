e9fe3e5a102ed6c76127aa2ee3a2c49320bfaca5
Bote Zhou <botez4204@gmail.com>
2025-02-22 14:24:37 -0500

    add background image

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
-import './style.css'
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+import './style.css';
 
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight,
+  height: window.innerHeight
 }
 
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
-  canvas: gameCanvas
+  canvas: gameCanvas,
+  scene: {
+    preload: preload,
+    create,
+    update
+  }
 }
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('beach', 'assets/images/beach_background_1.jpg');
+}
+
+function create() {
+  this.add.image(400, 300, 'beach');
+}
+
+function update() {
+
+}
